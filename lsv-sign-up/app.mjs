6670582053556594bb22f3c63e0eabffcef21341
Paul Laney <paul.laney@movember.com>
2023-04-21 12:45:39 +1000

    Updated code and removed some template

--- lsv-sign-up/app.mjs
+++ lsv-sign-up/app.mjs
@@ -2,16 +2,23 @@ import { CognitoIdentityProviderClient, SignUpCommand } from "@aws-sdk/client-co
 
 // Set the AWS region
 const region = 'ap-southeast-2';
-const userPoolId = 'ap-southeast-2_OTygxTTfD'
-const clientId = 'kd6r7hdl67ds3fevm7rfc1h5b';  
+const userPoolId = 'ap-southeast-2_JooVrMDto'
+const clientId = 'hsrgcarl1k9t7jrujdg9v61jj';  
 
 // Export the handler
 export const handler = async (event) => {
   console.log(event);
 
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const name = document.getElementById('name').value;
+  const client = new CognitoIdentityProviderClient({
+    region: region
+  });
+
+  console.log(JSON.parse(event.body));
+
+  const payLoad = JSON.parse(event.body);
+  const email = payLoad["email"];
+  const password = payLoad["password"];
+  const name = payLoad["name"];
   // Define the user attributes
   const params = {
     ClientId: clientId,
@@ -21,10 +28,6 @@ export const handler = async (event) => {
       {
         Name: "email",
         Value: email,
-      },
-      {
-        Name: "name",
-        Value: name,
       }
     ],
   };
@@ -34,48 +37,11 @@ export const handler = async (event) => {
 
   try {
     const response = await client.send(signUpCommand);
-    console.log("Sign up successfully:", data);
+    console.log("Sign up successfully:", response);
     return { statusCode: 200, body: "Sign up successfully" };
-    console.log(response);
+
   } catch (error) {
     console.error("Error Signing up:", error);
     return { statusCode: 500, body: "Error Signing up" };
-  }
-
-
-
-
-
-
-
-  const client = new DynamoDBClient({ region: REGION });
-  const params = {
-    TableName: TABLE_NAME,
-    Item: {
-      "questionId" : {S:"123"},
-      "question" : {S: "this is a question"},
-      "correctAnswer" : {S : "3"},
-      "media": {S : "https://somepath" },
-      "answers" : {M : 
-        {
-          1 : { S: "Answer One"} ,
-          2 : { S:  "Answer Two"},
-          3 : { S:  "Answer Three"},
-          4 : { S:  "Answer Four"},
-          5 : { S:  "Answer Five"},
-        }
-      }
-    }
-  };
-
-  const command = new PutItemCommand(params);
-
-  try {
-    const data = await client.send(command);
-    console.log("Item inserted successfully:", data);
-    return { statusCode: 200, body: "Item inserted successfully" };
-  } catch (error) {
-    console.error("Error inserting item:", error);
-    return { statusCode: 500, body: "Error inserting item" };
   }
 };
